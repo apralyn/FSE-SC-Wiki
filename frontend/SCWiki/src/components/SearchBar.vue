669589bf6496f68db636c3c525086bd6475b6b2c
apralyn eribal <aeribal@wgu.edu>
2023-04-30 00:44:42 -0700

    no wrap on search bar

---
+++ frontend/SCWiki/src/components/SearchBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="search-bar">
+    <input class="search" type="Text" placeholder="Search">
+    <button>Search</button>
+  </div>
+</template>
+<style>
+.search-bar {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  margin: auto;
+  margin-top: 25px;
+}
+
+.search {
+  max-width: 75vw;
+  min-width: 65vw;
+  height: 85px;
+  background-color: white;
+  border: 1px solid white;
+  border-radius: 10px 0 0 10px;
+}
+
+button {
+  background-color: white;
+  width: 65px;
+  height: 85px;
+  border: 1px solid white;
+  border-left: 2px solid rgba(88, 89, 88, 0.508);
+  border-radius: 0 10px 10px 0;
+}
+</style>
--- frontend/SCWiki/src/views/HomeView.vue
+++ frontend/SCWiki/src/views/HomeView.vue
@@ -2,11 +2,19 @@
   <div class="home">
     <h1>Show Control Wiki Page</h1>
     <div class="search-bar">
-      <input class="search" type="Text" placeholder="Search">
-      <button>Search</button>
+      <SearchBar />
     </div>
   </div>
 </template>
+<script>
+import SearchBar from "../components/SearchBar.vue"
+export default {
+  name: "HomeView",
+  components: {
+    SearchBar,
+  }
+}
+</script>
 <style scope>
 .home {
   height: 100vh;
@@ -15,32 +23,11 @@
   display: flex;
   flex-direction: column;
   align-items: center;
-
 }
 
 h1 {
   text-align: center;
   padding: 20px;
   color: white;
-}
-
-.serach-bar {
-  display:block;
-  flex-direction: row;
-}
-
-.search {
-  width: 75vw;
-  height: 65px;
-  background-color: white;
-  border: 1px solid white;
-}
-
-button {
-  background-color: white;
-  width: 50px;
-  height: 65px;
-  border: 1px solid white;
-  border-left: 2px solid greenyellow;
 }
 </style>
